8a39494e8524c714442d7ee6a598c7a18b3cef61
Merlin GRIMALDI <mgrimald@e2r12p2.42.fr>
2015-01-10 18:51:42 +0100

    deja plus fonctionnel

--- get_next_line.c
+++ get_next_line.c
@@ -1,73 +1,33 @@
 #include "get_next_line.h"
 
-int				get_next_line(const int fd, char **line)
+static t_list	*gnl_rest(char *rst)
 {
-	t_lst	*lst;
-	char	*str;
-	int		rd;
-
-	rd = 1;
-	if (line == NULL)
-		return (-1);
-	if ((lst = gnl_rest) == NULL)
-	{
-		str = ft_strnew(BUFF_SIZE);
-		if ((rd = read(fd, str, BUFF_SIZE)) =< 0)
-			return (rd);
-		lst = ft_lstnew(str, rd);
-	}
-	return (gnl_act(fd, line, &lst, str));
-}
-
-static t_lst	*gnl_rest(char *rst)
-{
-	static t_lst	*rest = NULL;
-	t_lst			tmp;
+	static t_list	*rest = NULL;
+	t_list			*tmp;
 
 	if (rst != NULL)
 	{
 		if (ft_strlen(rst) != 0)
 			rest = ft_lstnew(rst, ft_strlen(rst));
+		else
+			rest = NULL;
+	}
+	else if (rest)
+	{
+		tmp = ft_memdup(rest, sizeof(t_list));
+		free(rest);
+		rest = NULL;
 	}
 	else
-	{
-		tmp = ft_memcpy(rest);
-		free(rest);
-	}
+		tmp = NULL;
 	return (tmp);
 }
 
-static int		gnl_act(int fd, char **line, t_lst **alst, char *str)
-{
-	int		rd;
-	t_lst	*lst;
-	char	*cmp;
-	int		i;
-
-	rd = 1;
-	while (rd >= 0)
-	{
-		if ((i = ft_strclen((*alst)->content)) >= 0 || rd = 0)//presence de \n
-		{
-			if (i < 0)
-				i = ft_strlen()
-			cmp = ft_(alst);
-			cmp[i] = '\0';
-			*line = strdup(cmp);
-			gnl_rest(cmp + i + 1);
-			free(cmp);
-		}
-		lst = ft_lstnew(str, rd);
-		ft_lstadd(alst, lst);
-	}
-	return (1);
-}
-
-gnl_lsttochar(t_lst **alst)
+static char		*gnl_lsttochar(t_list **alst)
 {
 	char	*str;
 	int		size;
-	t_lst	*lst;
+	t_list	*lst;
 
 	size = 0;
 	lst = *alst;
@@ -80,7 +40,55 @@ gnl_lsttochar(t_lst **alst)
 	str = ft_strnew(size);
 	while (lst)
 	{
-		lst = lst->next;
-		ft_lstdel(alst, del(void*, size_t));
+		size = size - lst->content_size;
+		ft_memcpy(str + size, lst->content, lst->content_size);
+		ft_lstdelone(&lst, del);
 	}
+	return (str);
+}
+
+static int		gnl_act(int fd, char **line, t_list **alst, char *str)
+{
+	int		rd;
+	int		i;
+
+	rd = 1;
+	while (rd >= 0)
+	{
+		if ((i = ft_strclen((*alst)->content, '\n')) >= 0 || rd == 0)
+		{
+			if (i >= 0)
+			{
+				gnl_rest((*alst)->content + i + 1);
+				((char*)(*alst)->content)[i] = '\0';
+				(*alst)->content_size = i;
+			}
+			*line = gnl_lsttochar(alst);
+			free(str);
+			return (1);
+		}
+		if ((rd = read(fd, str, BUFF_SIZE)) < 0)
+			return (rd);
+		ft_lstadd(alst, ft_lstnew(str, rd));
+	}
+	return (-1);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	t_list	*lst;
+	char	*str;
+	int		rd;
+
+	rd = 1;
+	if (line == NULL)
+		return (-1);
+	str = ft_strnew(BUFF_SIZE);
+	if ((lst = gnl_rest(NULL)) == NULL)
+	{
+		if ((rd = read(fd, str, BUFF_SIZE)) <= 0)
+			return (rd);
+		lst = ft_lstnew(str, rd);
+	}
+	return (gnl_act(fd, line, &lst, str));
 }
